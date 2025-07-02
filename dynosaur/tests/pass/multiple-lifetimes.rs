bd24e98c102f79e8d0e0bde8fb44fede2f3d689d
Santiago Pastorino <spastorino@gmail.com>
2025-07-02 16:53:45 -0300

    Test: add lifetimnes outlives relationship in a function

--- dynosaur/tests/pass/multiple-lifetimes.rs
+++ dynosaur/tests/pass/multiple-lifetimes.rs
@@ -1,14 +1,14 @@
 #[dynosaur::dynosaur(DynSomeTrait)]
 trait SomeTrait {
     async fn multiple_elided_lifetimes(&self, a: &u8, b: &u8);
-    async fn multiple_named_lifetimes<'a, 'b>(&self, a: &'a u8, b: &'b u8, c: &u8);
+    async fn multiple_named_lifetimes<'a, 'b: 'a>(&self, a: &'a u8, b: &'b u8, c: &u8);
     async fn same_lifetimes_multiple_params<'a>(&self, a1: &'a u8, a2: &'a u8, c: &u8) -> &'a u8;
     async fn multiple_static_and_anonymous(&self, a: &'static u8, b: &'_ u8, c: &'_ u8);
 }
 
 impl SomeTrait for () {
     async fn multiple_elided_lifetimes(&self, a: &u8, b: &u8) {}
-    async fn multiple_named_lifetimes<'a, 'b>(&self, a: &'a u8, b: &'b u8, c: &u8) {}
+    async fn multiple_named_lifetimes<'a, 'b: 'a>(&self, a: &'a u8, b: &'b u8, c: &u8) {}
     async fn same_lifetimes_multiple_params<'a>(&self, a1: &'a u8, a2: &'a u8, c: &u8) -> &'a u8 {
         a2
     }
